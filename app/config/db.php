8da8440e0cb24adcee27b8d25c1d3d1605ea47ff
tte <tackanoway35@gmail.com>
2017-01-05 09:19:37 +0700

    First Commit

---
+++ app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=easyii_tte_travel',
+    'username' => 'root',
+    'password' => 'thanhno1',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => true,
+];
